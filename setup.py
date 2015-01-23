cb36aa20b1028bc787064e1bc05bcda6a7b2fb36
pirogoeth <pirogoeth@maio.me>
2015-01-22 23:24:56 -0600

    1.0: updated package base module and setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
                 'malibu.config',
                 'malibu.connection',
                 'malibu.database',
+                'malibu.text',
                 'malibu.util'],
     package_dir = {'malibu': 'malibu'},
     zip_safe = True
