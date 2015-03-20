e2eaf01e29af89c24933401a5d6625cbe0895dc8
pirogoeth <pirogoeth@maio.me>
2015-03-19 19:04:45 -0500

    1.0: added argparser tests

---
+++ tests/argparser_tests.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python2.7
+
+import contextlib, malibu, os, unittest
+from malibu.util.args import ArgumentParser
+from nose.tools import *
+
+class ArgumentParserTestCase(unittest.TestCase):
+
+    def argumentParserSingle_test(self):
+
+        args = ['-c']
+
+        ap = ArgumentParser(args, mapping = {'c' : 'create'})
+        ap.parse()
+
+        self.assertEquals(ap.options['create'], True)
+
+    def argumentParserParameterized_test(self):
+
+        args = ['-c', 'filename.txt']
+
+        ap = ArgumentParser(args, mapping = {'c' : 'create'})
+        ap.add_option_type('c', type = ArgumentParser.OPTION_PARAMETERIZED)
+        ap.parse()
+
+        self.assertEquals(ap.options['create'], 'filename.txt')
+
+    def argumentParserMultiple_test(self):
+
+        args = ['-c', 'filename.txt', '--syntax', 'plain', '-w']
+
+        ap = ArgumentParser(args)
+        ap.add_option_mapping('c', 'create')
+        ap.add_option_type('c', type = ArgumentParser.OPTION_PARAMETERIZED)
+
+        ap.add_option_type('syntax', type = ArgumentParser.OPTION_PARAMETERIZED)
+
+        ap.add_option_mapping('w', 'watch')
+        ap.parse()
+
+        self.assertEquals(ap.options['create'], 'filename.txt')
+        self.assertEquals(ap.options['syntax'], 'plain')
+        self.assertEquals(ap.options['watch'], True)
