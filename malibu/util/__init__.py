195378012bbb348a438590dc89168216e518609f
Sean Johnson <sean.johnsonProduct@rackspace.com>
2015-06-10 13:11:49 -0500

    0.1.3: Wow. Make tests pass again.

--- malibu/util/__init__.py
+++ malibu/util/__init__.py
@@ -1,4 +1,4 @@
-import glob, inspect, glob
+import glob, inspect, os
 
 modules = glob.glob(os.path.dirname(__file__) + "/*.py")
 __all__ = [os.path.basename(f)[:-3] for f in modules if not os.path.basename(f).startswith('_') and not f.endswith('__init__.py') and os.path.isfile(f)]
